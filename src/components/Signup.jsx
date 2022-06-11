21bbe5ea448608dd75572aba0c04b32d83d6d213
Gaurav Yadav <gk5480325@gmail.com>
2022-06-11 08:32:10 +0530

    Text Field has done

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import { Formik, Form } from 'formik';
+import TextField from './TextField';
 
 const Signup = () => {
     return (
-        <div>
-            <h1>Signup</h1>
-        </div>
+        <Formik
+            initialValues={{
+                firstName :'',
+                lastName : '',
+                email: '',
+                password: '',
+                confirmPassword: ''
+            }} >
+            {formik => (
+                <div>
+                    <h1 className="my-4 font-weight-bold-display-4">SignUp</h1>
+                    {console.log(formik)}
+                    <Form>
+                        <TextField label='First Name' name='firstName' type='text' />
+                        <TextField label='Last Name' name='lastName' type='text' />
+                        <TextField label='Email' name='email' type='email' />
+                        <TextField label='Password' name='password' type='password' />
+                        <TextField label='Confirm password' name='confirmpassword' type='password' />
+                    </Form> 
+                </div>
+            )}
+        </Formik>
     );
 }
 
---
+++ src/components/TextField.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useField } from 'formik';
+
+const TextField = ({ label, ...props}) => {
+    const [field, meta] = useField(props);
+    return (
+        <div className='mb-2'>
+            <label htmlFor={field.name}>{label}</label>
+            <input
+            className='form-control shadow-none'
+            {...field} {...props}
+            autoComplete='off'
+             />
+        </div>
+    );
+}
+
+export default TextField;
