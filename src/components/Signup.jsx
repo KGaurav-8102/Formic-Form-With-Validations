21e19ae8c72c9f6e0926c8fbffe7f2f501a38681
Gaurav Yadav <gk5480325@gmail.com>
2022-06-11 12:43:28 +0530

    Form with validation Finished

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
 import { Formik, Form } from 'formik';
 import TextField from './TextField';
+import * as Yup from 'yup';
 
 const Signup = () => {
+    const validate = Yup.object({
+        firstName:  Yup.string()
+        .max(15, 'Must  be 15 Character or less')
+        .required('Required'),
+        lastName:  Yup.string()
+        .max(20, 'Must  be 20 Character or less')
+        .required('Required'),
+        email:  Yup.string()
+        .email('Email is Invalid!')
+        .required('Email is Required'),
+        password:  Yup.string()
+        .min(6, 'Password must be atleast 6 Characters')
+        .required('Password is Required'),
+        confirmPassword:  Yup.string()
+        .oneOf([Yup.ref('password'), null], 'Password must match')
+        .required('Confirm password is Required')
+    })
+
     return (
         <Formik
             initialValues={{
@@ -11,17 +30,21 @@ const Signup = () => {
                 email: '',
                 password: '',
                 confirmPassword: ''
-            }} >
+            }}
+            validationSchema = {validate}
+            >
             {formik => (
                 <div>
                     <h1 className="my-4 font-weight-bold-display-4">SignUp</h1>
-                    {console.log(formik)}
+                    {console.log(formik.values)}
                     <Form>
                         <TextField label='First Name' name='firstName' type='text' />
                         <TextField label='Last Name' name='lastName' type='text' />
                         <TextField label='Email' name='email' type='email' />
                         <TextField label='Password' name='password' type='password' />
                         <TextField label='Confirm password' name='confirmpassword' type='password' />
+                        <button className="btn btn-dark mt-3" type='submit'>Register</button>
+                        <button className="btn btn-danger mt-3" type='reset' style={{ marginLeft: '8px'}}>Reset</button>
                     </Form> 
                 </div>
             )}
--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { useField } from 'formik';
+import { ErrorMessage, useField } from 'formik';
 
 const TextField = ({ label, ...props}) => {
     const [field, meta] = useField(props);
     return (
-        <div className='mb-2'>
+        <div className='mb-3'>
             <label htmlFor={field.name}>{label}</label>
             <input
-            className='form-control shadow-none'
+            className={`form-control shadow-none ${meta.touched && meta.error && 'is-invalid'}`}
             {...field} {...props}
             autoComplete='off'
              />
+             <ErrorMessage component='div' name={field.name} className="error" />
         </div>
     );
 }
